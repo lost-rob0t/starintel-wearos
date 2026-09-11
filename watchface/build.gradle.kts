c2bb46651bd4fad792742632c8954fd34266f234
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-11 09:15:00 -0400

    build: add Watch Face Format module

---
+++ watchface/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "actor.starintel.watchface"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "actor.starintel.watchface"
+        minSdk = 33
+        targetSdk = 37
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
